b97e0dbc2ff1892f3da4a8de5588728fdcc56eef
Abolfazl <abolfazl.hassanzade.81@gmail.com>
2022-02-17 20:51:24 +0330

    Created PhoneBook model without migrate and migrations

--- base/models.py
+++ base/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+
+class PhoneBook(models.Model):
+    phone_number = models.CharField(max_length=11)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    create = models.DateTimeField(auto_now_add=True)
+    
+
+    def __str__(self):
+        return self.phone_number
+
+    def __unicode__(self):
+        return self.phone_number
