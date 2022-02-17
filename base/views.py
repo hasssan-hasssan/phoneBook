4d1057c26f357f8284567e274a7dc1daf0329e51
Abolfazl <abolfazl.hassanzade.81@gmail.com>
2022-02-17 21:59:56 +0330

    Created getPhones views

--- base/views.py
+++ base/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from base.models import *
 
-# Create your views here.
+
+def getPhones(request):
+    phones = PhoneBook.objects.all().order_by('-create')
+    
+    context = {
+        'phones':phones,
+    }
+    
+    return render(request, 'index.html', context)
