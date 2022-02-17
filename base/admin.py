350fbe4425d46849dd41fb2711ccdec1d315b2cf
Abolfazl <abolfazl.hassanzade.81@gmail.com>
2022-02-17 20:58:32 +0330

    Registered PhoneBook Model to admin panel

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from base.models import PhoneBook
 
-# Register your models here.
+
+@admin.register(PhoneBook)
+class PhoneBookAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone_number','description','create')
+    list_filter = ['create']
+    ordering = ['-create']
